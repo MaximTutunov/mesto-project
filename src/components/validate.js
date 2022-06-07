85f8d00f2f65d9a354f64f4b01fed8e67e2bf17d
MaximTutunov <97736607+MaximTutunov@users.noreply.github.com>
2022-06-07 16:58:39 +0300

    1658

--- src/components/validate.js
+++ src/components/validate.js
@@ -1,84 +1,88 @@
 //Function Validation
 // Errors Display
-const displayInputError = (inputElement, { inputErrorClass, errorClass }) => {
-    const errorElement = inputElement
-      .closest(".popup__form")
-      .querySelector(`#${inputElement.id}-error`);
-    inputElement.classList.add(inputErrorClass);
-    errorElement.classList.add(errorClass);
-    errorElement.textContent = inputElement.validationMessage;
-  };
-  
-  //Errors hide
-  const hideInputError = (formIndividual, inputElement, subConfig) => {
-    const { inputErrorClass, errorClass } = subConfig;
-    const errorElement = formIndividual.querySelector(
-      `#${inputElement.id}-error`
-    );
-    inputElement.classList.remove(inputErrorClass);
-    errorElement.classList.remove(errorClass);
-    errorElement.textContent = "";
-  };
-  
-  //check for form validity
-  
-  const checkInputValidity = (formIndividual, inputElement, subConfig) => {
-    if (inputElement.validity.valid) {
-      hideInputError(formIndividual, inputElement, subConfig);
-    } else {
-      displayInputError(inputElement, subConfig);
-    }
-  };
-  
-  //button toggling
-  
-  const toggleButtonState = (
-    formIndividual,
-    buttonElement,
-    inactiveButtonClass
-  ) => {
-    const isFormValid = formIndividual.checkValidity();
-    buttonElement.classList.toggle(inactiveButtonClass, !isFormValid);
-    buttonElement.disabled = !isFormValid;
-  };
-  
-  // setting Listeners
-  
-  const setEventListeners = (formIndividual, subConfig) => {
-    const {
-      inputSelector,
-      submitButtonSelector,
-      inactiveButtonClass,
-      errorClass,
-      inputErrorClass,
-    } = subConfig;
-    const inputList = Array.from(formIndividual.querySelectorAll(inputSelector));
-  
-    const buttonElement = formIndividual.querySelector(submitButtonSelector);
-    toggleButtonState(formIndividual, buttonElement, inactiveButtonClass);
-    inputList.forEach((inputElement) => {
-      inputElement.addEventListener("input", () => {
-        checkInputValidity(formIndividual, inputElement, {
-          inputErrorClass,
-          errorClass,
-        });
-  
-        toggleButtonState(formIndividual, buttonElement, inactiveButtonClass);
-      });
-    });
-  };
-  
-  //enabling Validation on Submit
-  
-  const enableValidation = (subConfig) => {
-    const { formSelector, ...restProps } = subConfig;
-    const formsAll = Array.from(document.querySelectorAll(formSelector));
-  
-    formsAll.forEach((form) => {
-      form.addEventListener("submit", (evt) => evt.preventDefault());
-  
-      setEventListeners(form, restProps);
-    });
-  };
+const displayInputError = (
+  inputElement,
+  { inputErrorClass, errorClass, formSelector }
+) => {
+  const errorElement = inputElement
+    .closest(formSelector)
+    .querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.add(inputErrorClass);
+  errorElement.classList.add(errorClass);
+  errorElement.textContent = inputElement.validationMessage;
+};
 
-  export {enableValidation}
+//Errors hide
+const hideInputError = (formIndividual, inputElement, subConfig) => {
+  const { inputErrorClass, errorClass } = subConfig;
+  const errorElement = formIndividual.querySelector(
+    `#${inputElement.id}-error`
+  );
+  inputElement.classList.remove(inputErrorClass);
+  errorElement.classList.remove(errorClass);
+  errorElement.textContent = "";
+};
+
+//check for form validity
+
+const checkInputValidity = (formIndividual, inputElement, subConfig) => {
+  if (inputElement.validity.valid) {
+    hideInputError(formIndividual, inputElement, subConfig);
+  } else {
+    displayInputError(inputElement, subConfig);
+  }
+};
+
+//button toggling
+
+const toggleButtonState = (
+  formIndividual,
+  buttonElement,
+  inactiveButtonClass
+) => {
+  const isFormValid = formIndividual.checkValidity();
+  buttonElement.classList.toggle(inactiveButtonClass, !isFormValid);
+  buttonElement.disabled = !isFormValid;
+};
+
+// setting Listeners
+
+const setEventListeners = (formIndividual, subConfig) => {
+  const {
+    inputSelector,
+    submitButtonSelector,
+    inactiveButtonClass,
+  
+  } = subConfig;
+
+  const inputList = Array.from(formIndividual.querySelectorAll(inputSelector));
+
+  
+  const buttonElement = formIndividual.querySelector(submitButtonSelector);
+  toggleButtonState(formIndividual, buttonElement, inactiveButtonClass);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", () => {
+      checkInputValidity(formIndividual, inputElement, subConfig);
+
+      toggleButtonState(formIndividual, buttonElement, inactiveButtonClass);
+    });
+  });
+};
+
+//enabling Validation on Submit
+
+const enableValidation = (subConfig) => {
+  const { formSelector, ...restProps } = subConfig;
+  const formsAll = Array.from(document.querySelectorAll(formSelector));
+
+  formsAll.forEach((form) => {
+    form.addEventListener(
+      "submit",
+      setEventListeners(form, subConfig)
+    );
+    
+  });
+};
+
+export { enableValidation };
